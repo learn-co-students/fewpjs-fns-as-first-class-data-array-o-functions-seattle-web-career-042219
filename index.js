fae41617c310a71256de1712b9e18c6550a3965d
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-05 16:51:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 function wakeDog(dogName, dogBreed) {
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    let string = `Wake ${dogName} the ${dogBreed}`;
+    console.log(string)
+    return string;
   }
 function leashDog(dogName, dogBreed) {
-    console.log(`Leash ${dogName} the ${dogBreed}`);}
-
-function walkToPark(dogName, dogBreed) {
-    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    let string = `Leash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string
 }
 
+function walkToPark(dogName, dogBreed) {
+    let string = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+
 function throwFrisbee(dogName, dogBreed) {
-    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
 }
 
 function walkHome(dogName, dogBreed) {
-    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    let string = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(string)
+    return string;
 }
 
 function unleashDog(dogName, dogBreed) {
-    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    let string = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
 }
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed){
-    for(i=0; i<routine.length; i++){
-        routine[i](dogName, dogBreed);
-    }
+    let myArray = []
+    routine.forEach((element)=>{
+        myArray.push(element(dogName, dogBreed));
+    });
+    return myArray
 }
+
